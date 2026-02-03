242fbaeb4423235bb96f2fb5f6dc646182bfaa87
ishantiwariii <i.e.ishantiwari@gmail.com>
2026-02-03 22:32:27 +0530

    Fix DB config for Render

--- certsite/settings.py
+++ certsite/settings.py
@@ -54,14 +54,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'certsite.wsgi.application'
 
 
-# DATABASE (Auto switches to Render PostgreSQL in production)
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=not DEBUG
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # PASSWORD VALIDATION
